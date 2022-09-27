3fc0e1b882602298db713acebfdb8520edf5f989
Musa Uçar <92794324+musaucar@users.noreply.github.com>
2022-09-27 19:41:26 +0300

    react testing library
    react testing library works

---
+++ src/components/counter/counter.test.js
@@ -0,0 +1,37 @@
+import {render, screen} from "@testing-library/react"
+import userEvent from '@testing-library/user-event'
+ 
+import Counter from "./index"
+
+describe('Counter Tests', () =>{
+    let increaseBtn, decreaseBtn, count;
+
+    beforeEach(()=> {
+        console.log("her testten önce çalışacağım")
+        render(<Counter/>)
+        increaseBtn = screen.getByText('Increase');
+        decreaseBtn = screen.getByText('Decrease');
+        count = screen.getByText("0");
+    })
+    
+    beforeAll(()=>{
+        console.log("İlk başta bir kere çalışacağım")
+    })
+
+    afterAll(() =>{
+        console.log("en son bir kere çalışacağım")
+    })
+
+    afterEach(() => {
+        console.log("Her testten sonra çalışacağım");
+    })
+    test('increase btn', () => {
+        userEvent.click(increaseBtn);
+        expect(count).toHaveTextContent("1");
+    })
+    
+    test('decrease btn', () => {
+        userEvent.click(decreaseBtn);
+        expect(count).toHaveTextContent("-1");
+    })
+})
