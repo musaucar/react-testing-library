96e6a56c400ab7159ff26e1d714ce6e1a6c10901
Musa Uçar <92794324+musaucar@users.noreply.github.com>
2022-09-28 17:53:08 +0300

    react testing library
    react testing library update works

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-import Counter from './components/counter';
+//import Counter from './components/counter';
+import Todo from './components/Todo'
 
 function App() {
   return (
     <div className="App">
-      <Counter/>
+      {/* <Counter/> */}
+      <Todo/>
     </div>
   );
 }
